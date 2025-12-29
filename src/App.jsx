982e385f9ada2b5d514e17aae152d76daf26b52c
MADALA LEELA VENKATA SIVA SPOORTHI <mmadalavenkatesh@gmail.com>
2025-12-30 00:31:03 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar";
+import Hero from "./sections/Hero";
+import Skills from "./sections/Skills";
+import Projects from "./sections/Projects";
+import Contact from "./sections/Contact";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Skills />
+      <Projects />
+      <Contact />
+    </>
+  );
+}
