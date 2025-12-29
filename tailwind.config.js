715917a937b438cd3ac97f2054d81d05d191da30
MADALA LEELA VENKATA SIVA SPOORTHI <mmadalavenkatesh@gmail.com>
2025-12-30 00:27:45 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        neon: "#00F5D4",
+        glass: "rgba(255,255,255,0.08)"
+      }
+    }
+  },
+  plugins: []
+};
